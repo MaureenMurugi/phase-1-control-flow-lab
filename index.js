b0de64ee3ade131f00436d6f35d6fa0a8ea0abb8
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-08-17 07:11:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return "This one is on me!"
+  }if (feet >= 2000) {
+    return "I will gladly take your thirty bucks."
+  }if (feet >= 2500) {
+    return "No can do."
+  } 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+scuberGreetingForFeet(199);
+//scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
+
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
+}
+ternaryCheckCity("Pittsburgh");
+
+ternaryCheckCity('NYC');
+
+function switchOnCharmFromTip(tip){
+  if (tip === "generous") {
+    return "Thank you so much."
+  } if (tip === "not as generous") {
+    return "Thank you."
+  }else{
+    return "Bye."
+  }
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+switchOnCharmFromTip("generous");
